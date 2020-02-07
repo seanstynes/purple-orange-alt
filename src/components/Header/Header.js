9afd7c76c5820d8f970a513254049ee8e20aa9ca
Sean Stynes <sean@teambrightly.com>
2020-02-07 16:44:25 -0500

    Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { ReactComponent as Logo } from '../../img/orange.svg';
+// import { ReactComponent as Logo } from '../../img/orange.svg';
 
-import classes from 'classnames';
-import css from './Header.module.scss';
+// import classes from 'classnames';
+// import css from './Header.module.scss';
 
 const Header = class extends React.Component {
 	render() {
-		return (
-			<header className={classes(css.header)}>
-				<div className='container'>
-					<div className={css.inner}>
-						<Link to='/' className='inheritLink'>
-							<div className={css.logo}>
-								<Logo></Logo>
-							</div>
-							<h1>Purple Orange Studios</h1>
-						</Link>
-						<nav>
-							{/* <Link to='/listen'>Listen</Link> */}
-							<Link to='/blog'>Posts</Link>
-							<Link to='/reviews'>Reviews</Link>
-							<Link to='/contact'>Contact</Link>
-							{/* <Link to='/guides'>Guides</Link> */}
-						</nav>
-					</div>
-				</div>
-			</header>
-		);
+		return <div>Nav</div>;
 	}
 };
 
